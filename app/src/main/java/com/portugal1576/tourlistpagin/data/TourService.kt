d26fe0e447d56bd094ed03ad10a3b9aa97fe4619
Portugal1576 <45799586+Portugalandrii@users.noreply.github.com>
2021-01-27 11:14:25 +0200

    Delete adress

--- app/src/main/java/com/portugal1576/tourlistpagin/data/TourService.kt
+++ app/src/main/java/com/portugal1576/tourlistpagin/data/TourService.kt
@@ -10,16 +10,15 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 //анотация меняет поведение метода не меняя код
-//http://api.tourstart.org/api/list.tour/simpleAll
+
 interface TourService {
 
-    @GET("list.tour/simpleAll")  // запрос методом GET
-    fun getTour() : Single<Response>  //функция выводит объект класса Call,
-    // который содержит результат работы сервера
+    @GET("***************")  // запрос методом GET
+    fun getTour() : Single<Response>
 
-    companion object {   // сингитон - статический объект одиночка, для обращения к которому не требуется создание класса
 
-        var BASE_URL = "http://api.tourstart.org/api/"
+    companion object {
+        var BASE_URL = "*********"
 
         fun create(): TourService {    //эта функция реализует интерфейс BankService с помощью класса Retrofit
 
